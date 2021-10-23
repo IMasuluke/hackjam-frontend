f696ae9c81ef3036f709b1990f4091324ff1900c
KekePhasha <61992331+KekePhasha@users.noreply.github.com>
2021-10-23 17:14:06 +0200

    New web pages

---
+++ src/Pages/Finance/finance.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+import { Button,Container,Row, Col,Card, ListGroup, Carousel, CardGroup, } from 'react-bootstrap';
+
+
+class FinanceFrom extends Component {
+   
+
+     render() {
+         return (
+                <Container>
+                    <Row>
+                        <Col>
+                            <h1> Account</h1>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                        </Col>
+                    </Row>
+
+
+                </Container>
+
+             );
+     }
+}
+export default FinanceFrom;
