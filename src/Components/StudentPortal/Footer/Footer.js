dc2e6390c0549d83c097a8654e7a5e729e51fae4
KekePhasha <61992331+KekePhasha@users.noreply.github.com>
2021-11-03 22:19:16 +0200

    Adding sticky component

--- src/Components/StudentPortal/Footer/Footer.js
+++ src/Components/StudentPortal/Footer/Footer.js
@@ -4,54 +4,80 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button,Container,Row, Col, Image, Nav } from 'react-bootstrap';
 import { BsInstagram, BsTwitter, BsFacebook} from "react-icons/bs";
 import "./Footer.css";
+import Image2 from "../../../Images/EduvosLogo(Blue).png";
+import Logo from "../../../Images/Logos/Logo(Motto).png";
 
 class FooterForm extends Component {
     render (){
     return (
-        <Container  fluid>
-            <Row className= "Footer">
+       
+
+        <Container  fluid >
+             <Container className= "bots" >
+            <Row>
+                 <Col >
+                        <img class="rounded-circle"  src={Image2} alt="CampusImage"  />
+                </Col>
+            </Row>
+        </Container>
+        <Container  fluid className="Footer">
+            <Row  >
                     <Col className= "Contactus">
-                      <h3>Contact Us </h3> <br />
+                      <h3>Contact Us </h3> 
+                      <hr className= "line"/>
                         <h4>+27 11 341 4569</h4>
                         <h4>eduvos@eduvos.com</h4>
                     </Col>
                     < Col className= "FindUs">
-                        <h3>Find Us</h3> <br />
+                        <h3>Find Us</h3> 
+                         <hr className= "line"/>
                         <h4>44 Alsatian Road, Glen Austin Midrand,
                             Johanessburg South Africa
                         </h4>
                     </Col>
                     <Col className= "QuickLinks">
-                    <Nav>
+                    
                         <h3>Quick links</h3>
+                         <hr className= "line"/>
                         <h4>Career</h4> 
                         <h4>Eduvos Shop</h4>
                         <h4>FAQ</h4>
-                    </Nav>
+                    
                     </Col>
                     <Col className= "FollowUs"> 
-                        <h3>Follow us on Social Media</h3> <br />
+                        
+                        <h3>Follow us on Social Media</h3> 
+                         <hr className= "line"/>
                         <Row className= "Icons">
                             <Col>
-                            <BsInstagram href= "/ReturningStudentWelcome" target="_blank" rel="noopener noreferre" />
-                            </Col> 
-                            <Col><BsTwitter href= "/ReturningStudentWelcome" target="_blank" rel="noopener noreferre"/>
-                            </Col>
-                            <Col>
-                            <BsFacebook href= "/ReturningStudentWelcome" target="_blank" rel="noopener noreferre"/>
+                            < a  href= "https://www.instagram.com/eduvoseducation/" target="_blank" rel="noopener noreferre" style= {{color: "white"}}> <BsInstagram size= {30} /> </a> {' '}
+                            < a href= "https://twitter.com/edu_vos" target="_blank" rel="noopener noreferre" style= {{color: "white"}}> <BsTwitter size= {30} /> </a> {' '}
+                                                      
+                            < a href= "https://www.facebook.com/EduvosEducationSA/" target="_blank" rel="noopener noreferre" style= {{color: "white"}}> <BsFacebook size= {30} /> </a>
                             </Col>
                         </Row>
                         <Row>
                             <Col>
-                            <h3>Privacy Policy | POPI | Terms & Conditions</h3>
+                            <h4> Privacy Policy | POPI | Terms & Conditions
+
+                            </h4>
                             </Col>
                         </Row>
                     </Col>
+                    <Col >
+                        {/* <img className="Image2" src={Image2} alt="CampusImage" fluid /> */}
+                    </Col>
                     
+                </Row>
+                <Row>
+                    <Col>
+                     keke
+                                          
+                    </Col>
                 </Row>
                  
         
-         
+         </Container>
         </Container>
     );
 
